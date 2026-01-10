41814572ff743ce9a4e427a71a90b6008fdb79ff
telefon <kheltan.engineering@gmail.com>
2026-01-10 15:57:53 +0100

    feat: hello world example

---
+++ algorithms/hello-world.py
@@ -0,0 +1,13 @@
+text = "hello, stranger!"
+
+G = engine.DiGraph([(i + 1, i + 2) for i in range(len(text) - 1)])
+
+G.color_edges_by(lambda u, v, G: v if "label" in G.nodes[v] else None)
+G.color_nodes_by(lambda v, G: v if "label" in G.nodes[v] else None)
+G.label_nodes_by(lambda v, G: G.nodes[v]["label"] if "label" in G.nodes[v] else " ")
+
+for i, symbol in enumerate(text):
+    G.nodes[(i + 1)]["label"] = symbol
+
+print(text)
+
